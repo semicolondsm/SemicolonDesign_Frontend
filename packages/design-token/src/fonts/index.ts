2b9c8bf0fb3dac82820b128fb8ce2f4b2bd3dd03
Eungyeol <66717787+eungyeole@users.noreply.github.com>
2021-10-20 02:38:53 +0900

    feat(design-token): colors 및 fonts 추가 (#7)

---
+++ packages/design-token/src/fonts/index.ts
@@ -0,0 +1,9 @@
+import { FontWeightScheme } from './types'
+
+/* test */
+export const weight: Readonly<FontWeightScheme> = {
+    light: '200',
+    regular: '300',
+    medium: '400',
+    bold: '500'
+}
