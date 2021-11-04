3085c0331d041854dbd0e04b8ab2c30002cdbac8
Eungyeol <66717787+eungyeole@users.noreply.github.com>
2021-11-04 22:25:23 +0900

    chore: esbuild config 파일 정리 (#24)

--- packages/design-token/esbuild.config.js
+++ packages/design-token/esbuild.config.js
@@ -1,16 +1,14 @@
 const esbuild = require('esbuild');
 const { pnpPlugin } = require('@yarnpkg/esbuild-plugin-pnp');
+const pkg = require('./package.json');
 
 esbuild
     .build({
         entryPoints: ['./src/index.ts'],
-        outfile: 'dist/index.js',
-        bundle: true,
-        minify: true,
-        platform: 'browser',
+        outfile: pkg.main,
         format: 'cjs',
         sourcemap: true,
-        target: 'es6',
+        bundle: true,
         plugins: [pnpPlugin()],
     })
     .catch(() => process.exit(1));
