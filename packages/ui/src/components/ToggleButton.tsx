8896a168e7f444b6194d34d69007d7a8185fe964
Sung Ye In <syi9397@naver.com>
2021-11-04 12:56:57 +0900

    feat(ui): toggleButton 및 Test Story 페이지 추가

---
+++ packages/ui/src/components/ToggleButton.tsx
@@ -0,0 +1,107 @@
+import React, { FC, CSSProperties, useState } from 'react';
+import styled from  '@emotion/styled';
+import { Botton } from './typography';
+
+type ClickEvent = React.MouseEvent<HTMLButtonElement, MouseEvent>;
+
+interface itemOption {
+    title: string;
+    onClick?: (event: ClickEvent) => void;
+    buttonStyle?: CSSProperties;
+}
+
+interface ButtonProps {
+    items: itemOption[];
+    containStyle?: CSSProperties;
+}
+
+interface ButtonStyledProps {
+    activeNumber: number;
+    length: number;
+}
+
+const Container = styled.div`
+    width: 100%;
+    padding: 4px;
+    overflow: hidden;
+    border-radius: 22px;
+    display: flex;
+    position: relative;
+`;
+
+const ButtonWrapper = styled.div`
+    width: 100%;
+    display: flex;
+    position: relative;
+`;
+
+const Button = styled.button`
+    border-radius: 22px;
+    padding: 8px 0;
+    flex: 1;
+    cursor: pointer;
+    border: none;
+    background: none;
+`;
+
+const ActiveBackground = styled.div<ButtonStyledProps>`
+    width: calc(${(props) => 100 / props.length}%);
+    height: 100%;
+    border-radius: 22px;
+    position: absolute;
+    background: white;
+    z-index: -1;
+    transform: translateX(${(props) => (props.activeNumber - 1) * 100}%);
+    transition: all .3s cubic-bezier(0.5, 1, 0.89, 1);
+`;
+
+// left: ${(props) => (props.activeNumber - 1) * 100 / props.length}%;
+// 
+
+const Background = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: ${(props) => props.theme.colors.gray100};
+    z-index: -2;
+`;
+
+const ToggleButton: FC<ButtonProps> = ({
+    items,
+    containStyle
+}) => {
+    const [activeNumber, setActiveNumber] = useState<number>(1);
+
+    const buttonOnClick = (number: number, callback: ((event: ClickEvent) => void) | undefined, e: ClickEvent) => {
+        setActiveNumber(number);
+        if(callback) callback(e);
+    }
+
+    return (
+        <Container style={containStyle}>
+            <Background />
+            <ButtonWrapper>
+                <ActiveBackground activeNumber={activeNumber} length={items.length} />
+                {
+                    items.map(({
+                        title,
+                        onClick,
+                        buttonStyle,
+                    }, i) => (
+                        <Button 
+                            style={buttonStyle} 
+                            onClick={(e) => buttonOnClick(i+1, onClick, e)} 
+                            key={i}
+                        >
+                            <Botton color="gray700">{title}</Botton>
+                        </Button>
+                    ))
+                }
+            </ButtonWrapper>
+        </Container>
+    )
+}
+
+export default ToggleButton;    
