72b7e9e2554a45890618b5501d0b9d32740b627b
kimulchan <leerabi577@gmail.com>
2021-12-09 22:47:56 +0900

    docs(ui): SwitchButton docs 작성

--- packages/ui/src/components/SwitchButton.stories.tsx
+++ packages/ui/src/components/SwitchButton.stories.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
 import { SwitchButton } from './SwitchButton';
-export default { title: 'components/SwitchButton' };
-export const Test = () => {
+export default { 
+    title: 'components/SwitchButton',
+    component: SwitchButton,
+    parameters:{
+        componentSubtitle:"스위치 버튼 컴포넌트"
+    },
+    argTypes: {
+         styletype: { control: 
+            { 
+                type: 'select', 
+                options: [ 'primary', 'secondary', ]
+            } 
+        } 
+    }
+
+ };
+export const SwitchButtonStory = () => {
     const [value, setValue] = React.useState<boolean>(false);
     return (
         <>
